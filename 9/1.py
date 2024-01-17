87beee196ca38223314fe26824ae96d2e35ad734
leprikon8888 <leprikon8888@gmail.com>
2024-01-17 19:31:49 +0200

    УРА!!!!!!!!

--- 9/1.py
+++ 9/1.py
@@ -1,3 +1,65 @@
 '''Створіть абстрактний базовий клас "Фігура" і від нього успадкуйте конкретні класи, такі як "Коло", "Прямокутник",
  "Трикутник" і т.д. Кожен клас має мати методи для обчислення площі та периметру фігури. Створіть декілька об'єктів
  різних фігур і виведіть їх площу та периметр.'''
+from abc import ABC, abstractmethod
+import math
+
+
+class Figura(ABC):
+
+    @abstractmethod
+    def perimetr(self):
+        ...
+
+    @abstractmethod
+    def area(self):
+        ...
+
+
+class Kolo(Figura):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def perimetr(self):
+        return 2 * math.pi * self.radius
+
+    def area(self):
+        return math.pi * self.radius * self.radius
+
+
+class Rect(Figura):
+    def __init__(self, a, b):
+        self.a, self.b = a, b
+
+    def perimetr(self):
+        return 2 * (self.a + self.b)
+
+    def area(self):
+        return self.a * self.b
+
+
+class Triangle(Figura):
+    def __init__(self, a, b, c):
+        self.a, self.b, self.c = a, b, c
+
+    def perimetr(self):
+        return self.a + self.b + self.c
+
+    def area(self):
+        x = (self.a + self.b + self.c) / 2
+        return math.sqrt(x * (x - self.a) * (x - self.b) * (x - self.c))
+
+
+if __name__ == "__main__":
+
+    a = Kolo(5)
+    print(a.perimetr())
+    print(a.area())
+
+    b = Rect(2, 5)
+    print(b.perimetr())
+    print(b.area())
+
+    c = Triangle(3, 7, 9)
+    print(c.perimetr())
+    print(c.area())
--- 9/2.py
+++ 9/2.py
@@ -3,3 +3,60 @@
 специфіки кожного платіжного засобу. Створіть клас "ПлатіжнийПроцесор", який містить список доступних платіжних засобів
 та метод для виконання платежу через вибраний засіб. Можна створити об'єкти різних платіжних засобів, додати їх до
 платіжного процесора і здійснити платежі через них'''
+
+
+class PaymentInstrument:
+
+    def make_a_payment(self):
+        raise NotImplementedError('нужно реализовать')
+
+
+class CreditCart(PaymentInstrument):
+    def make_a_payment(self):
+        return 'payment by credit card'
+
+
+class BankTransfer(PaymentInstrument):
+    def make_a_payment(self):
+        return 'payment by bank transfer'
+
+
+class ElectronicWallet(PaymentInstrument):
+    def make_a_payment(self):
+        return 'payment by electronic money'
+
+
+class PayProcessor:
+
+    def __init__(self):
+        self.list_instruments = []
+
+    def add_instrument(self, instrument: PaymentInstrument):
+        if not isinstance(instrument, PaymentInstrument):
+            raise TypeError('ожидаем другой тип данных')
+        elif not instrument in self.list_instruments:
+            self.list_instruments.append(instrument)
+        else:
+            raise Exception("этот платежный метод уже есть в списке")
+
+    def run_pay(self, instrument: PaymentInstrument):
+        if instrument in self.list_instruments:
+            print(instrument.make_a_payment())
+        else:
+            raise ValueError('Pay error, pay instument is not defined')
+
+
+if __name__ == "__main__":
+
+    cred = CreditCart()
+    bank = BankTransfer()
+    e_wallet = ElectronicWallet()
+
+    proc = PayProcessor()
+
+    proc.add_instrument(cred)
+    proc.add_instrument(bank)
+    proc.add_instrument(e_wallet)
+
+    proc.run_pay(cred)
+    proc.run_pay(bank)
